c365d34437c730d4dd3bcf9d8f675f4b6a1702c7
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-04 19:12:27 -0500

    taller_2

---
+++ Taller Estructuras de Control Secuenciales/7_Ejercicio.py
@@ -0,0 +1,6 @@
+metro=float(input("Ingrese la cantidad de metros:"))
+
+pulgada=metro*39.27
+pie=pulgada/12
+
+print(metro, " metros equivalen a ", pulgada, " pulgadas y ", pie, " pies")
---
+++ Taller Estructuras de Control Secuenciales/9_Ejercicio.py
@@ -0,0 +1,8 @@
+numero_hora_trabajada=float(input("Ingrese numero de horas trabajadas:"))
+precio_hora_trabajada=float(input("Ingrese el precio de la hora: "))
+
+
+salario_neto=(numero_hora_trabajada*precio_hora_trabajada)-0.20
+
+
+print("El salario neto es", salario_neto)
