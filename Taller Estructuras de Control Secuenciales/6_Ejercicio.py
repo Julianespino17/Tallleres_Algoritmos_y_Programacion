b10769ece0d16058ab03999a5bb5d27df406095f
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-05 19:13:10 -0500

    Delete 6_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/6_Ejercicio.py
+++
@@ -1,9 +0,0 @@
-hombres=int(input("Ingrese el numero de hombres:"))
-mujeres=int(input("Ingrese el numero de mujeres:"))
-
-total=hombres+mujeres
-hombre_procentaje=(hombres/total)*100
-mujeres_procentaje=(mujeres/total)*100
-
-print("El porcentajes de hombres es ", hombre_procentaje, "%")
-print("El porcentajes de mujeres es ", mujeres_procentaje, "%")
