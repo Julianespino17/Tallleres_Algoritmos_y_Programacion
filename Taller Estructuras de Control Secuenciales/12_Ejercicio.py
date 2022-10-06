3ee529ecbaa3d3ba0cdb8c1e7285131c6dd04f96
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-05 19:04:28 -0500

    continuacion taller2

---
+++ Taller Estructuras de Control Secuenciales/12_Ejercicio.py
@@ -0,0 +1,41 @@
+#entradas
+#notas matematicas
+examen_matematicas=float(input("Ingrese el examen de matematicas:")) 
+tarea1_matematicas=float(input("Ingrese la tarea 1 de matematicas:"))
+tarea2_matematicas=float(input("Ingrese la tarea 2 de matematicas:"))
+tarea3_matematicas=float(input("Ingrese la tarea 3 de matematicas:"))
+
+#notas fisica
+examen_fisica=float(input("Ingrese el examen de fisica:")) 
+tare1_fisica=float(input("Ingrese la tarea 1 de fisica:"))
+tarea2_fisica=float(input("Ingrese la tarea 2 de fisica:"))
+
+#notas quimica
+examen_quimica=float(input("Ingrese el examen de quimica:")) 
+tarea1_quimica=float(input("Ingrese la tarea 1 de quimica:"))
+tarea2_quimica=float(input("Ingrese la tarea 2 de quimica:"))
+tarea3_quimica=float(input("Ingrese la tarea 3 de quimica:"))
+
+
+#caja negra
+#promedios notas matematicas
+prom_tareas_matematicas=(tarea1_matematicas+tarea2_matematicas+tarea3_matematicas)/3  
+prom_matematicas=((examen_matematicas*0.90)+(prom_tareas_matematicas*0.10))
+
+#promedios notas fisica
+prom_tarea_fisica=(tare1_fisica+tarea2_fisica)/2     
+prom_fisica=((examen_fisica*0.80)+(prom_tarea_fisica*0.20))
+
+#promedios notas quimica
+prom_tareas_quimica=(tarea1_quimica+tarea2_quimica+tarea3_quimica)/3  
+prom_quimica=((examen_quimica*0.90)+(prom_tareas_quimica*0.10))
+
+#promedio general
+prom_general=((prom_matematicas+prom_fisica+prom_quimica)/3)
+
+
+#salidas
+print("El promedio de matematicas es de: ",prom_matematicas)
+print("El promedio de fisica es de: ",prom_fisica)
+print("El promedio de quimica es de: ",prom_quimica)
+print("El promedio general de las tres materias es de: ",prom_general)
--- Taller Estructuras de Control Secuenciales/9_Ejercicio.py
+++ Taller Estructuras de Control Secuenciales/9_Ejercicio.py
@@ -2,7 +2,7 @@ numero_hora_trabajada=float(input("Ingrese numero de horas trabajadas:"))
 precio_hora_trabajada=float(input("Ingrese el precio de la hora: "))
 
 
-salario_neto=(numero_hora_trabajada*precio_hora_trabajada)-0.20
+salario_neto=(numero_hora_trabajada*precio_hora_trabajada)*0.80
 
 
 print("El salario neto es", salario_neto)
