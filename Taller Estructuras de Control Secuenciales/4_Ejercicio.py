302dd87ec947ca1f15a21f92fb254ed1ab868a4e
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-05 19:12:33 -0500

    Delete 4_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/4_Ejercicio.py
+++
@@ -1,6 +0,0 @@
-total_compra=float(input("Digite el total de su compra:"))
-
-descuento=total_compra*0.15
-descuento_total=(total_compra-descuento)
-
-print("el descuento hecho fue de:",descuento,"Su total a pagar es de:",descuento_total)
