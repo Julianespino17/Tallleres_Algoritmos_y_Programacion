8adbda22ec6b67d33400285e81f4b297a2bc7918
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-05 19:11:27 -0500

    Delete 1__Ejercicio.py

--- Taller Estructuras de Control Secuenciales/1__Ejercicio.py
+++
@@ -1,5 +0,0 @@
-Edad_uno=int(input("Digite la primera edad:"))
-Edad_dos=int(input("Digite la segundo edad:"))
-Edad_tres=int(input("Digite la tercera edad:"))
-Promedio=(Edad_uno+Edad_dos+Edad_tres)/3
-print("Este es el promedio:",Promedio)
