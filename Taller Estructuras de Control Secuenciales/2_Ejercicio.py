0d36318d8ba6bbf762c086eba0f96ec319ee935e
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-05 19:11:57 -0500

    Delete 2_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/2_Ejercicio.py
+++
@@ -1,4 +0,0 @@
-inversio_banco=float(input("Digite la inversion que le deposito al banco:"))
-dinero_ganado=(inversio_banco)*0.2
-dinero_total=(inversio_banco+dinero_ganado)
-print("La ganancia de su inversion es:",dinero_ganado,"Su total es",dinero_total)
