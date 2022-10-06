da6a852fb5451868c15905a002deac4eadf9bbaa
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-05 19:09:01 -0500

    Delete 8_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/8_Ejercicio.py
+++
@@ -1,9 +0,0 @@
-import math
-#Entradas
-lado_1=float(input("Ingrese lado 1: "))
-lado_2=float(input("Ingrese lado 2: "))
-lado_3=float(input("Ingrese lado 3: "))
-#Caja negra
-s= (lado_1+lado_2+lado_3)/2
-area= math.sqrt(s*(s-lado_1)*(s-lado_2)*(s-lado_3))
-print("El area es: "+str(area))
