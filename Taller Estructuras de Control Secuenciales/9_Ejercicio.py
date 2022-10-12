42b7ed7aee32a83bfb59b08b78ef12496c129443
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-11 20:28:55 -0500

    Update 9_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/9_Ejercicio.py
+++ Taller Estructuras de Control Secuenciales/9_Ejercicio.py
@@ -1,8 +1,7 @@
+#entrada
 numero_hora_trabajada=float(input("Ingrese numero de horas trabajadas:"))
 precio_hora_trabajada=float(input("Ingrese el precio de la hora: "))
-
-
+#caja negra
 salario_neto=(numero_hora_trabajada*precio_hora_trabajada)*0.80
-
-
+#salida
 print("El salario neto es", salario_neto)
