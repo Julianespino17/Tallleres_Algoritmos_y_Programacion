35b422739953b736b0842cd40be497bf07df8ecc
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-11 20:24:50 -0500

    Update 2_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/2_Ejercicio.py
+++ Taller Estructuras de Control Secuenciales/2_Ejercicio.py
@@ -1,4 +1,7 @@
+#entrada
 inversio_banco=float(input("Digite la inversion que le deposito al banco:"))
+#caja negra
 dinero_ganado=(inversio_banco)*0.2
 dinero_total=(inversio_banco+dinero_ganado)
+#salida
 print("La ganancia de su inversion es:",dinero_ganado,"Su total es",dinero_total)
