b199e44bca3ded8518438fa2262e304738647107
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-11 20:37:16 -0500

    Update 19_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/19_Ejercicio.py
+++ Taller Estructuras de Control Secuenciales/19_Ejercicio.py
@@ -1,10 +1,9 @@
+#entrada
 Lote_de_naranjas=int(input("Lote de naranjas: "))
 Docena=float(input("Docena: "))
 total_ventas=float(input("Cantidad de las ventas: "))
-
-
+#caja negra
 total_ganancias=(Lote_de_naranjas/12)*Docena
 porcentaje_ganancias=round((total_ventas-total_ganancias)*100)/total_ganancias
-
-
+#salida
 print("El porcentaje de ganancia fue de ",porcentaje_ganancias, "%")
