7ae07d0b5f9b27836263c27a3ae385c8903ea134
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-11 20:38:00 -0500

    Update 21_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/21_Ejercicio.py
+++ Taller Estructuras de Control Secuenciales/21_Ejercicio.py
@@ -1,12 +1,9 @@
-
-
+#entrada
 precio_computador=float(input("Ingrese el precio del computador: "))
 valor_cuotas=float(input("Ingrese el valor de las cuotas: "))
-
-
+#caja negro
 total_cuotas=valor_cuotas*12
 resta=total_cuotas-precio_computador
 porcentaje_recargo=(resta*100)/precio_computador
-
-
+#salida
 print("Su porcentaje de recargo es del ", porcentaje_recargo, "%")
