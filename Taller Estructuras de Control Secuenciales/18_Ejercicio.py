c8e6bdc101cfadfa35d77be56aa9777bb50258c4
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-11 20:36:32 -0500

    Update 18_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/18_Ejercicio.py
+++ Taller Estructuras de Control Secuenciales/18_Ejercicio.py
@@ -1,8 +1,7 @@
+#entrada
 capital=float(input("Ingrese el capital "))
 tasa_interes=float(input("Ingrese la tasa de interes "))
-
-
+#caja negra
 i=(tasa_interes*100)/(capital*4)
-
-
+#salida
 print("Su porcentaje anual de cobro por el prestamo de ",capital, " sera de ",i, "%")
