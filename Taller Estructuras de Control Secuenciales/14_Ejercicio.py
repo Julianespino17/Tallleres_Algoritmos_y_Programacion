efecc4d2b8f0af7d8068c117d185dc7f3a022b06
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-11 20:32:47 -0500

    Update 14_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/14_Ejercicio.py
+++ Taller Estructuras de Control Secuenciales/14_Ejercicio.py
@@ -1,7 +1,8 @@
+#entrada
 costo_kilovatio=int(input("Ingrese el costo por kilovatio: "))
 Lectura_Anterior=int(input("Ingrese la lectura anterior en kilovatios:"))
 Lectura_Actual=int(input("Ingrese la lectura actual en kilovatios:"))
-
+#caja negra
 T=(Lectura_Actual-Lectura_Anterior)/costo_kilovatio
-
+#salida
 print("Su monto total a pagar es de:",T)
