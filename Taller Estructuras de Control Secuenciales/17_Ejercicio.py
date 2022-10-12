2959fd4f537d2b2329cc959488a7502a14abda7f
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-11 20:34:56 -0500

    Update 17_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/17_Ejercicio.py
+++ Taller Estructuras de Control Secuenciales/17_Ejercicio.py
@@ -1,12 +1,10 @@
-
+#entrada
 presupuesto_anual=float(input("Ingrese el presupuesto anual: "))
-
-
+#caja negra
 presupuesto_ginecologia=(presupuesto_anual)*0.40
 presupuesto_traumatologia=(presupuesto_anual)*0.30
 presupuesto_pediatria=(presupuesto_anual)*0.30
-
-
+#salida
 print("Ginecologia recibira un presupuesto de: ",presupuesto_ginecologia)
 print("Traumatologia recibira un presupuesto de: ",presupuesto_traumatologia)
 print("Pediatria recibira un presupuesto de:", presupuesto_pediatria)
