22b520b3f50763fadd9c8d175f7a27f4fc375e69
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-11 20:27:56 -0500

    Update 7_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/7_Ejercicio.py
+++ Taller Estructuras de Control Secuenciales/7_Ejercicio.py
@@ -1,6 +1,7 @@
+#entrada
 metro=float(input("Ingrese la cantidad de metros:"))
-
+#caja negra
 pulgada=metro*39.27
 pie=pulgada/12
-
+#salida
 print(metro, " metros equivalen a ", pulgada, " pulgadas y ", pie, " pies")
