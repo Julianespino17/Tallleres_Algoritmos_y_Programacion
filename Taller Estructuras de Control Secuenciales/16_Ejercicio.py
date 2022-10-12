f6062abf09035b3d2934509c8f1dfa7de679bbe2
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-11 20:34:10 -0500

    Update 16_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/16_Ejercicio.py
+++ Taller Estructuras de Control Secuenciales/16_Ejercicio.py
@@ -1,9 +1,8 @@
 import graphlib
-
-
+#entrada
 Galones_Registrados=int(input("Ingrese la cantidad de galones registrados"))
-
+#caja negra
 S=Galones_Registrados*3.785
 A=S*50000
-
+#salida
 print("se le tiene que cobrar al cliente:", A)
