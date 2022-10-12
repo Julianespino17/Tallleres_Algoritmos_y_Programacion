e0237b8c52fe747a386d6c4a41aa6f7f775a7e2a
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-11 20:26:05 -0500

    Update 4_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/4_Ejercicio.py
+++ Taller Estructuras de Control Secuenciales/4_Ejercicio.py
@@ -1,6 +1,7 @@
+#entrada
 total_compra=float(input("Digite el total de su compra:"))
-
+#caja negra
 descuento=total_compra*0.15
 descuento_total=(total_compra-descuento)
-
+#salida
 print("el descuento hecho fue de:",descuento,"Su total a pagar es de:",descuento_total)
