7fa21921323fbe5b037f30abed9d9aa1fd99d525
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-11 20:33:41 -0500

    Update 15_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/15_Ejercicio.py
+++ Taller Estructuras de Control Secuenciales/15_Ejercicio.py
@@ -1,6 +1,7 @@
+#entrada
 PVP=int(input("Ingrese el precio del producto sin descuento:"))
 PVF=int(input("Ingrese el precio pagado por el producto con el descuento:"))
-
+#caja negra
 Desc=100-((PVF*100)/PVP)
-
+#salida
 print("El porcentaje del descuento hecho al producto es de:", Desc)
