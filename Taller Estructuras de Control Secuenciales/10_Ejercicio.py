9f0e8512e1463139c32d4c2b946ee1a79ae47158
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-11 20:29:42 -0500

    Update 10_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/10_Ejercicio.py
+++ Taller Estructuras de Control Secuenciales/10_Ejercicio.py
@@ -1,14 +1,13 @@
+#entradas
 chelines_austriacos=float(input("Ingrese la cantidad de chelines austriacos: "))
 dragmas_griegos=float(input("Ingrese la cantidad de dragmas griegos: "))
 pesetas=float(input("Ingrese la cantidad de pesetas: "))
-
-
+#caja negra
 Chelines_austriacos_pesetas=chelines_austriacos*9.56
 Dragmas_griegos_francos_franceses=((chelines_austriacos*0.956)/20.110)
 Pesetas_dolares=pesetas/122.499
 Pesetas_liras_intalianas=pesetas/0.092289
-
-
+#salida
 print(chelines_austriacos, " chelines equivalen a", Chelines_austriacos_pesetas, " pesetas")
 print(dragmas_griegos, " dragmas griegos equivalen a", Dragmas_griegos_francos_franceses, " francos franceses")
 print(pesetas, " pesetas equivalen a", Pesetas_dolares, " dolares y ", Pesetas_liras_intalianas, " liras italianas")
