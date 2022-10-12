4dfbb880ef107066240820164979e3f4d2122f51
Julian Espinosa <114430887+Julianespino17@users.noreply.github.com>
2022-10-11 20:32:15 -0500

    Update 13_Ejercicio.py

--- Taller Estructuras de Control Secuenciales/13_Ejercicio.py
+++ Taller Estructuras de Control Secuenciales/13_Ejercicio.py
@@ -1,4 +1,4 @@
-
+#entrada
 N1=int(input("Ingrese la cantidad de billetes de 50000:"))
 N2=int(input("Ingrese la cantidad de billetes de 20000:"))
 N3=int(input("Ingrese la cantidad de billetes de 10000:"))
@@ -7,7 +7,7 @@ N5=int(input("Ingrese la cantidad de billetes de 2000:"))
 N6=int(input("Ingrese la cantidad de billetes de 1000:"))
 N7=int(input("Ingrese la cantidad de billetes de 500:"))
 N8=int(input("Ingrese la cantidad de billetes de 100: "))
-
+#caja negra
 A=N1*50000
 B=N2*20000
 C=N3*10000
@@ -17,5 +17,5 @@ F=N6*1000
 G=N7*500
 H=N8*100
 Dinero_Total=A+B+C+D+E+F+G+H 
-
+#salida
 print("La cantidad de dinero en el banco es de:", Dinero_Total)
